d9dde308d08f17fbfb900a994671d862b6182065
roy <roy@roys-MacBook-Air.local>
2025-03-04 23:13:49 +0530

    folder refactor

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -4,14 +4,13 @@ export default function Login() {
     return (
       <div className="h-screen flex items-center justify-center bg-gray-100">
         <div className="p-10 bg-white rounded-lg shadow-lg">
-          <h1 className="text-3xl mb-6 text-center">Register</h1>
+          <h1 className="text-3xl mb-6 text-center">Login</h1>
           <form className="flex flex-col space-y-4">
-            <input type="text" placeholder="Full Name" className="p-3 border rounded-lg" />
             <input type="email" placeholder="Email" className="p-3 border rounded-lg" />
             <input type="password" placeholder="Password" className="p-3 border rounded-lg" />
-            <button className="bg-purple-600 text-white p-3 rounded-lg">Register</button>
+            <button className="bg-purple-600 text-white p-3 rounded-lg">Login</button>
           </form>
-        <Link href="/login" className="text-center block mt-7 "> Already a User ? Click Here</Link>
+          <Link href="/user/register" className="text-center block mt-7"> New User ? Click Here</Link>
         </div>
       </div>
     );
